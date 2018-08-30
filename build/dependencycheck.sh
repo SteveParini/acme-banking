592becfdc108d0d6acedbfe64ac288dd2b3c7063
Ulisses Albuquerque <ulisses.montenegro@gmail.com>
2018-08-29 17:32:03 -0700

    Switch to CLI OWASP Dependency Check

--- build/dependencycheck.sh
+++ build/dependencycheck.sh
@@ -1,15 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check whether we are inside the Jeniks environment or not
-[ -z "${WORKSPACE}" ] && echo 'WORKSPACE is not defined.' && exit
+# Configurable parameters
+OWASP_DC_URL='http://dl.bintray.com/jeremy-long/owasp/dependency-check-3.3.1-release.zip'
 
-mkdir -p "${WORKSPACE}/reports/owasp-dependency-check"
-chmod -R 777 "${WORKSPACE}/reports/owasp-dependency-check"
+# Check if we are running under Jenkins
+if [ -z "${WORKSPACE}" ] ; then
+  echo 'This script is meant to executed by Jenkins'
+  exit 1
+fi
 
-mkdir -p "${WORKSPACE}/OWASP-Dependency-Check/data"
-chmod -R 777 "${WORKSPACE}/OWASP-Dependency-Check/data"
-
+# Download OWASP Dependency Check if it's not available in build agent
+if [ ! -d "${WORKSPACE}/dependency-check" ] ; then
+  echo 'Downloading OWASP Dependency Check'
+  wget -O /tmp/owasp-dc.zip "${OWASP_DC_URL}"
+  unzip -d "${WORKSPACE}" /tmp/owasp-dc.zip
+  rm /tmp/owasp-dc.zip
+fi
 
 # Run OWASP Dependency Check Dockerfile
-docker pull owasp/dependency-check
-docker run --rm --volume ${WORKSPACE}:/src --volume "${WORKSPACE}/OWASP-Dependency-Check/data":/usr/share/dependency-check/data --volume "${WORKSPACE}/reports/owasp-dependency-check":/report owasp/dependency-check --scan /src/package.json --format "XML" --project "acme-banking" -o /report -n
+# TODO: Remove hard-coded proxy information, parse it from ${http_proxy}
+/bin/sh "${WORKSPACE}/dependency-check/bin/dependency-check.sh" \
+  --project ${JOB_NAME} \
+  --format ALL \
+  --out "${WORKSPACE}/reports/owasp-dependency-check" \
+  --scan $(pwd) \
+  --proxyserver 172.17.0.1 --proxyport 3128 \
+  --disableBundleAudit
